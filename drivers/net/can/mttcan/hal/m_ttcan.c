2a94e590307b174d4d2e2f4aa2acd9100ea1708e
Skrilax_CZ <skrilax@gmail.com>
2025-02-25 22:52:26 -0800

    Tegra prod settings - use "legacy" version for newer kernels
    It would appear that "upstream" was not accepted nor available to
    use, therefore keep 5.15.0 kernel version

--- drivers/net/can/mttcan/hal/m_ttcan.c
+++ drivers/net/can/mttcan/hal/m_ttcan.c
@@ -291,7 +291,6 @@ inline u32 ttcan_read_ecr(struct ttcan_controller *ttcan)
 	return ttcan_read32(ttcan, ADR_MTTCAN_ECR);
 }
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 16, 0)
 static void tegra_mttcan_config_prod_settings(struct mttcan_priv *priv)
 {
 	struct ttcan_controller *ttcan = priv->ttcan;
@@ -315,48 +314,6 @@ static void tegra_mttcan_config_prod_settings(struct mttcan_priv *priv)
 	if (ret == 0)
 		dev_dbg(priv->device, "setting prod: %s\n", prod_name);
 }
-#else
-static void tegra_mttcan_write_prod_settings(struct mttcan_priv *priv, const char *prod_name)
-{
-	struct ttcan_controller *ttcan = priv->ttcan;
-	struct tegra_prod_reg_info *reg_info;
-	struct tegra_prod_cfg_info *prod_cfg;
-	u32 rval;
-	int i;
-
-	prod_cfg = tegra_prod_get_by_name_from_list(priv->device, ttcan->prod_list, prod_name);
-	if (prod_cfg == NULL)
-		return;
-
-	reg_info = prod_cfg->reg_info;
-	for (i = 0; i < prod_cfg->num_reg_info; ++i) {
-		rval = ttcan_read32(ttcan, reg_info[i].reg_offset);
-		rval &= ~reg_info[i].reg_mask;
-		rval |= reg_info[i].reg_value;
-		ttcan_write32(ttcan, reg_info[i].reg_offset, rval);
-	}
-}
-
-static void tegra_mttcan_config_prod_settings(struct mttcan_priv *priv)
-{
-	struct ttcan_controller *ttcan = priv->ttcan;
-	char *prod_name;
-
-	switch (ttcan->bt_config.data.bitrate) {
-	case MTTCAN_SPEED_5MBPS:
-		prod_name = "prod_c_can_5m";
-		break;
-	case MTTCAN_SPEED_8MBPS:
-		prod_name = "prod_c_can_8m";
-		break;
-	default:
-		prod_name = "prod_c_can_2m_1m";
-		break;
-	}
-
-	tegra_mttcan_write_prod_settings(priv, prod_name);
-}
-#endif
 
 int ttcan_set_bitrate(struct mttcan_priv *priv)
 {
--- include/linux/tegra-oot-prod.h
+++ include/linux/tegra-oot-prod.h
@@ -8,19 +8,10 @@
 
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 16, 0)
-#ifdef CONFIG_TEGRA_PROD_ENABLE
-#include <soc/tegra/tegra-prod.h>
-#include <linux/tegra_prod.h>
-#else
-#include <linux/tegra-prod-upstream-dummy.h>
-#include <linux/tegra-prod-legacy-dummy.h>
-#endif
-#else /* LINUX_VERSION_CODE */
 #ifdef CONFIG_TEGRA_PROD_ENABLE
 #include <linux/tegra_prod.h>
 #else
 #include <linux/tegra-prod-legacy-dummy.h>
 #endif
-#endif /* LINUX_VERSION_CODE */
+
 #endif /* _TEGRA_OOT_PRODS_H */
